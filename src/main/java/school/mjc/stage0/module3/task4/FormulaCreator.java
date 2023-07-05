92bf2a67541dfd3d52e3e2d41cd7c3188a938a58
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-07-05 17:44:59 +0600

    FormulaCreator class hasn't finished yet

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a = 10, b = 8;
+        double result = 0;
+        result = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * Math.round((a + b - 4 * a * b)/ 2);
+        System.out.println("Result: " + result);
+        /*double a = 0.0, b = 0.0, result = 0.0;
+        result = ((9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * (a + b - 4 * a * b) * 1.0) / 2;
+        System.out.println(result);*/
     }
 }
